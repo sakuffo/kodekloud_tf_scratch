364e7699bb0f746fd0b3a1a76c3fe6ee403b6be8
Stephen Akuffo <sa.akuffo@gmail.com>
2020-12-29 19:23:23 -0500

    iam

--- aws_tf_modules/iam.tf
+++ aws_tf_modules/iam.tf
@@ -5,3 +5,13 @@ resource "aws_iam_user" "admin-user" {
       course = "kodekloud"
   }
 }
+
+resource "aws_iam_policy" "adminUser" {
+  name = "AdminUsers"
+  policy =file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
+  user = aws_iam_user.admin-user.name
+  policy_arn = aws_iam_policy.adminUser.arn
+}
