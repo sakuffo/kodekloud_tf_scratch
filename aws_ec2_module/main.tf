eef18625e0576df2202c252ebe7c2981dbc70d5d
Stephen Akuffo <sa.akuffo@gmail.com>
2021-01-01 13:23:46 -0500

    modules module

--- aws_ec2_module/main.tf
+++ aws_ec2_module/main.tf
@@ -1,32 +1,56 @@
 resource "aws_instance" "webserver" {
-    ami = "ami-0885b1f6bd170450c" 
-    instance_type = "t3.micro"
-    tags = {
-      "Name" = "webserver"
-      "course" = "kodekloud"
-    }
-    user_data = <<EOF
-                #!/bin/bash
-                sudo apt update
-                sudo apt install nginx -y
-                systemctl enable nginx
-                systemctl start nginx
-                EOF
+  ami           = "ami-0885b1f6bd170450c"
+  instance_type = "t3.micro"
+  tags = {
+    "Name"   = "webserver"
+    "course" = "kodekloud"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install nginx -y",
+      "sudo systemctl enable nginx",
+      "sudo systemctl start nginx",
+    ]
+  }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.webserver.public_ip} >> ./ips.txt"
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("~/.ssh/tf_ec2")
+  }
+  #user_data
+  # user_data = <<EOF
+  #             #!/bin/bash
+  #             sudo apt update
+  #             sudo apt install nginx -y
+  #             systemctl enable nginx
+  #             systemctl start nginx
+  #             EOF
 
-    key_name = aws_key_pair.web.id
+  key_name               = aws_key_pair.web.key_name
+  vpc_security_group_ids = [aws_security_group.ssh-access.id]
 }
 
 resource "aws_key_pair" "web" {
+  key_name   = "tf_ec2"
   public_key = file("~/.ssh/tf_ec2.pub")
 }
 
 resource "aws_security_group" "ssh-access" {
-  name = "ssh-access"
+  name        = "ssh-access"
   description = "AllowSSH access from the Internet"
-  ingress{
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
   }
+}
+
+output "web_ip" {
+  value = aws_instance.webserver.public_ip
 }
